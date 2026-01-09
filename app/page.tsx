260dece210e26ae2054443fc282e49cbe4edd994
Bryan Hohs <bryanchohs@alpha.limo>
2026-01-08 20:22:33 -0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+export default function Alpha() {
+  return (
+    <div className="Alpha">
+      <header className="Alpha-header">
+        <img src="/logo.svg" className="Alpha-logo" alt="logo" />
+        {}
+        <p>
+          Alpha Limo
+        </p>
+        {}
+        <a
+          className="Alpha-link"
+          href="mailto:info@alpha.limo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        &copy; 2026 Alpha Limo LLC
+        </a>
+      </header>
+    </div>
+  )
+}
